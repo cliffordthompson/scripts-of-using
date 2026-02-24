5f096956cf01138f51d821add4042245c5887cb3
Clifford Thompson <clifford.thompson@gmail.com>
2026-02-24 09:50:03 -0800

    master: Added BAT script to clean up Visual Assist install

---
+++ clean_vax_license.bat
@@ -0,0 +1,40 @@
+REM --------------------------------------------------------------
+REM Cleans up files and registry entries left behind by Visual Assist.
+REM Use this if you want to free up space on your drive, and ensure
+REM your registry does not contain unused registry entries.
+REM --------------------------------------------------------------
+
+@echo off
+set tempfile="%temp%\1489AFE4.TMP"
+set regkey=HKEY_CURRENT_USER\SOFTWARE\Licenses
+set has_error=0
+
+tasklist | find /I "devenv.exe" > NUL
+if %errorlevel% == 0 (
+    echo Please close Visual Studio and try again.
+    exit /B 1
+)
+
+echo.
+
+echo Cleaning up Visual Assist X files and registry entries
+echo Deleting registry key %regkey% ...
+reg delete %regkey% /f
+if %errorlevel% == 1 (
+    echo Registry key was not deleted.
+    set has_error=1
+)
+echo.
+
+echo Deleting temporary file %tempfile%
+del %tempfile%>NUL
+
+echo.
+
+if %has_error% == 0 (
+    echo Done. Visual Assist temp files and registry entry removed
+) else (
+    echo Done. However, maybe not everything worked out right.
+)
+
+exit /B %has_error%
